2d8668787340b4fd82c5ed1af4cb82565d1fd261
lisastriker <lisastriker@gmail.com>
2025-10-13 23:39:31 +0800

    Added deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base:"mission-matching",
+    base:"/mission-matching/",
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
